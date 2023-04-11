903a62e80017495ebeddf5a1c876b4e24fe6a1c9
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-04-11 17:24:15 +0530

    Update virtual_machines.tf

--- GCP/virtual_machines.tf
+++ GCP/virtual_machines.tf
@@ -1,1 +1,22 @@
 
+resource "google_compute_instance" "default" {
+
+  name         = var.server_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  labels = {
+  }
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 15
+    }
+  }
+
+  network_interface {
+    network    = var.network_name
+    network_ip = var.statics_ip
+    subnetwork = var.subnet_name
+  }
+}
