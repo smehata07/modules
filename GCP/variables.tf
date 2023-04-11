01b68f4a27593f50853057ddbe71a709134ef7a3
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-04-11 17:27:51 +0530

    Update variables.tf

--- GCP/variables.tf
+++ GCP/variables.tf
@@ -1,1 +1,23 @@
+variable "network_name" {
+  type = string
+}
+variable "static_ip" {
+  type = string
+}
 
+variable "zone" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string 
+
+}
+variable "server_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+
+}
