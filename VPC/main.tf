cf1dbcaa8fa1389f6204b9cf98b244592640cd1b
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-08-20 13:13:57 +0530

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,1 +1,11 @@
+resource "google_compute_network" "custom-test" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
 
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = var.subnet_name
+  ip_cidr_range = var.ip_range
+  network       = google_compute_network.custom-test.id
+}
